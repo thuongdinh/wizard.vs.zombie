115b75b2eb9c4fbf3666413fe55bb6c032e946c9
thuongdinh <thuong.dinhhoang@asnet.com.vn>
2011-11-27 10:16:21 +0700

    Adding iOS version

---
+++ iOS/wvsz/wvsz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wvsz
+//
+//  Created by Thuong Dinh Hoang on 11/26/11.
+//  Copyright AsNet 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
